8e19b2afd6d6d7d386f2a5c8ac2a1f92e25e215b
Greg Karékinian <greg@karekinian.com>
2013-05-23 10:18:26 +0200

    Remove extra whitespace

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,8 @@
-default['chruby']['version'] = '0.3.4' 
-default['chruby']['gpg_check'] = false 
+default['chruby']['version'] = '0.3.4'
+default['chruby']['gpg_check'] = false
 default['chruby']['use_rvm_rubies'] = false
-default['chruby']['use_rbenv_rubies'] = false 
+default['chruby']['use_rbenv_rubies'] = false
 default['chruby']['auto_switch'] = true
-default['chruby']['rubies'] = ['1.9.3-p392'] 
+default['chruby']['rubies'] = ['1.9.3-p392']
 default['chruby']['default'] = 'embedded'
 default['chruby']['user_rubies'] = {}
