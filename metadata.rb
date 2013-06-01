17da4262139859dc9839b1757eda92c1c09d3ce9
Stephen Nelson-Smith <stephen@atalanta-systems.com>
2013-06-01 20:17:38 +0100

    Update metadata for version and support

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "support@atalanta-systems.com"
 license          "Apache 2.0"
 description      "Installs/Configures chruby"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.3"
+version          "0.1.4"
 depends		 "ark"
 depends		 "ruby_build"
 supports	 "centos"
