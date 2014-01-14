1719652784e19d87dc165fb9d4b503ab3cafb873
pseudomuto <david.muto@gmail.com>
2014-01-14 15:18:13 -0500

    adding user_rubies attribute for custom paths

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,6 +5,6 @@ default['chruby']['use_rbenv_rubies'] = false
 default['chruby']['auto_switch'] = true
 default['chruby']['rubies'] = {'1.9.3-p392' => true}
 default['chruby']['default'] = 'embedded'
-default['chruby']['user_rubies'] = {}
+default['chruby']['user_rubies'] = []
 default['chruby']['sh_dir'] = "/etc/profile.d"
 default['chruby']['sh_name'] = 'chruby.sh'
